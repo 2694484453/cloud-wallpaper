cd0a809d5b6ccfe5d278504a840f58461cc51b37
gaopuguang <2694484453@qq.com>
2026-01-19 22:47:05 +0800

    调整

--- src/pages/prometheus/alert/index.vue
+++ src/pages/prometheus/alert/index.vue
@@ -16,30 +16,26 @@
             <!--            <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出配置</t-button>-->
             <!--            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>-->
           </div>
-          <t-col :span="3">
-            <t-form-item label="名称" name="name">
-              <t-input v-model="searchForm.job" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
-                <template #suffix-icon>
-                  <search-icon size="20px"/>
-                </template>
-              </t-input>
-            </t-form-item>
-          </t-col>
-<!--          <t-col :span="3">-->
-<!--            <t-form-item label="类型" name="type">-->
-<!--              <t-select-->
-<!--                v-model="formData.type"-->
-<!--                :style="{ width: '200px' }"-->
-<!--                placeholder="请选择类型"-->
-<!--                class="demo-select-base"-->
-<!--                clearable-->
-<!--              >-->
-<!--                <t-option v-for="(item, index) in typeList" :key="index" :value="item" :label="item">-->
-<!--                  {{ item }}-->
-<!--                </t-option>-->
-<!--              </t-select>-->
-<!--            </t-form-item>-->
-<!--          </t-col>-->
+          <t-input v-model="searchForm.job" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
+            <template #suffix-icon>
+              <search-icon size="20px"/>
+            </template>
+          </t-input>
+          <!--          <t-col :span="3">-->
+          <!--            <t-form-item label="类型" name="type">-->
+          <!--              <t-select-->
+          <!--                v-model="formData.type"-->
+          <!--                :style="{ width: '200px' }"-->
+          <!--                placeholder="请选择类型"-->
+          <!--                class="demo-select-base"-->
+          <!--                clearable-->
+          <!--              >-->
+          <!--                <t-option v-for="(item, index) in typeList" :key="index" :value="item" :label="item">-->
+          <!--                  {{ item }}-->
+          <!--                </t-option>-->
+          <!--              </t-select>-->
+          <!--            </t-form-item>-->
+          <!--          </t-col>-->
           <!--        <t-input v-model="searchValue" class="search-input" placeholder="请输入你需要搜索的内容" clearable>-->
           <!--          <template #suffix-icon>-->
           <!--            <search-icon size="20px"/>-->
@@ -63,24 +59,24 @@
           :headerAffixedTop="true"
           :headerAffixProps="{ offsetTop: offsetTop, container: getContainer }"
         >
-          <template #health="{row}">
-            <t-tag v-show="row.health==='up'" theme="success" variant="light">up</t-tag>
-            <t-tag v-show="row.health==='wait'" theme="warning" variant="light">wait</t-tag>
-            <t-tag v-show="row.health==='down'" theme="danger" variant="light">down</t-tag>
+          <template #status="{row}">
+            <t-tag v-show="row.status === '' || row.status === null " theme="warning" variant="light">未知</t-tag>
+            <t-tag v-show="row.status === 'resolved'" theme="default" variant="light">已恢复</t-tag>
+            <t-tag v-show="row.status === 'firing'" theme="danger" variant="light">触发中</t-tag>
           </template>
           <template #globalUrl="{row}">
-            <a :href="row.globalUrl" target="_blank">{{row.globalUrl}}</a>
+            <a :href="row.globalUrl" target="_blank">{{ row.globalUrl }}</a>
           </template>
           <template #labels="{row}">
             <div v-for="(v,k) in row.labels">
-              <t-space >
-                <t-tag theme="primary" variant="light">{{k}}:{{v}}</t-tag>
+              <t-space>
+                <t-tag theme="primary" variant="light">{{ k }}:{{ v }}</t-tag>
               </t-space>
             </div>
           </template>
           <template #op="slotProps">
             <a class="t-button-link" @click="handleClickDetail(slotProps.row)">详情</a>
-            <a class="t-button-link" @click="handleClickDelete(slotProps.row)">删除</a>
+            <a class="t-button-link" @click="handleClickDelete(slotProps.row)">已读</a>
           </template>
         </t-table>
       </div>
@@ -115,59 +111,55 @@
       :onConfirm="handleDrawerOk"
       @cancel="onCancelDrawer"
     >
-<!--      <t-space v-show="drawer.operation === 'add'|| drawer.operation ==='edit'" direction="vertical" style="width: 100%">-->
-<!--        <t-form-->
-<!--          ref="formValidatorStatus"-->
-<!--          :data="formData"-->
-<!--          :label-width="100"-->
-<!--          @reset="onReset"-->
-<!--        >-->
-<!--          <t-form-item label="id" name="id" v-show="false">-->
-<!--            <t-input v-model="formData.id" :maxlength="32" with="120"></t-input>-->
-<!--          </t-form-item>-->
-<!--          <t-form-item label="名称" name="name">-->
-<!--            <t-input v-model="formData.name" placeholder="请输入仓库名称" :maxlength="64" with="120"></t-input>-->
-<!--          </t-form-item>-->
-<!--          <t-form-item label="类型" name="type">-->
-<!--            <t-input v-model="formData.type" placeholder="请输入仓库名称" :maxlength="64" with="120"></t-input>-->
-<!--          </t-form-item>-->
-<!--          <t-form-item label="地址" name="url">-->
-<!--            <t-input v-model="formData.url" placeholder="请输入仓库地址" :maxlength="64" with="120"></t-input>-->
-<!--          </t-form-item>-->
-<!--        </t-form>-->
-<!--      </t-space>-->
-<!--      <t-space v-show="drawer.operation === 'detail'" direction="vertical" style="width: 100%">-->
-<!--        <t-descriptions  bordered :layout="'vertical'" :item-layout="'horizontal'" :column="2">-->
-<!--          <t-descriptions-item label="名称">-->
-<!--            <t-space>-->
-<!--              <t-image fit="cover" :style="{width:'32px',height:'32px'}" :src="formData.icon"/>-->
-<!--            </t-space>-->
-<!--            {{formData.name}}-->
-<!--          </t-descriptions-item>-->
-<!--          <t-descriptions-item label="类型">{{formData.type}}</t-descriptions-item>-->
-<!--          <t-descriptions-item label="主页"><a :href="formData.home">{{formData.home}}</a></t-descriptions-item>-->
-<!--          <t-descriptions-item label="描述">{{formData.description}}</t-descriptions-item>-->
-<!--          <t-descriptions-item label="地址"><a :href="formData.url">{{formData.url}}</a></t-descriptions-item>-->
-<!--          <t-descriptions-item label="上架时间">{{formData.createTime}}</t-descriptions-item>-->
-<!--          <t-descriptions-item label="上架人">{{formData.createBy}}</t-descriptions-item>-->
-<!--          <t-descriptions-item label="更新时间">{{formData.updateTime}}</t-descriptions-item>-->
-<!--          <t-descriptions-item label="更新人">{{formData.updateBy}}</t-descriptions-item>-->
-<!--        </t-descriptions>-->
-<!--      </t-space>-->
-<!--      <t-space v-show="drawer.operation === 'install'" direction="vertical" style="width: 100%">-->
-<!--        <t-form-->
-<!--          ref="formValidatorStatus"-->
-<!--          :data="drawer.dynamicForm"-->
-<!--          :label-width="100"-->
-<!--          @reset="onReset"-->
-<!--        >-->
-<!--          <div v-for="(v,k) in drawer.dynamicForm">-->
-<!--            <t-form-item :label="k" :name="k">-->
-<!--              <t-input v-model="drawer.dynamicForm[k]" :type="typeof drawer.dynamicForm[k]" placeholder="请输入仓库名称" :maxlength="64" with="120" ></t-input>-->
-<!--            </t-form-item>-->
-<!--          </div>-->
-<!--        </t-form>-->
-<!--      </t-space>-->
+      <t-space v-show="drawer.operation === 'add'|| drawer.operation ==='edit'" direction="vertical"
+               style="width: 100%">
+        <t-form
+          ref="formValidatorStatus"
+          :data="formData"
+          :label-width="100"
+          @reset="onReset"
+        >
+          <t-form-item label="id" name="id" v-show="false">
+            <t-input v-model="formData.id" :maxlength="32" with="120"></t-input>
+          </t-form-item>
+          <t-form-item label="名称" name="name">
+            <t-input v-model="formData.name" placeholder="请输入仓库名称" :maxlength="64" with="120"></t-input>
+          </t-form-item>
+          <t-form-item label="类型" name="type">
+            <t-input v-model="formData.type" placeholder="请输入仓库名称" :maxlength="64" with="120"></t-input>
+          </t-form-item>
+          <t-form-item label="地址" name="url">
+            <t-input v-model="formData.url" placeholder="请输入仓库地址" :maxlength="64" with="120"></t-input>
+          </t-form-item>
+        </t-form>
+      </t-space>
+      <t-space v-show="drawer.operation === 'detail'" direction="vertical" style="width: 100%">
+        <t-descriptions bordered :layout="'vertical'" :item-layout="'horizontal'" :column="2">
+          <t-descriptions-item label="告警名称">{{ formData.alertName }}</t-descriptions-item>
+          <t-descriptions-item label="类型">{{ formData.exporterType }}</t-descriptions-item>
+          <t-descriptions-item label="分组名称">{{ formData.groupName }}</t-descriptions-item>
+          <t-descriptions-item label="状态">{{ formData.status }}</t-descriptions-item>
+          <t-descriptions-item label="等级">{{ formData.alertLevel }}</t-descriptions-item>
+          <t-descriptions-item label="创建时间">{{ formData.createTime }}</t-descriptions-item>
+          <t-descriptions-item label="归属人">{{ formData.createBy }}</t-descriptions-item>
+          <t-descriptions-item label="描述">{{ formData.description }}</t-descriptions-item>
+        </t-descriptions>
+      </t-space>
+      <t-space v-show="drawer.operation === 'install'" direction="vertical" style="width: 100%">
+        <t-form
+          ref="formValidatorStatus"
+          :data="drawer.dynamicForm"
+          :label-width="100"
+          @reset="onReset"
+        >
+          <div v-for="(v,k) in drawer.dynamicForm">
+            <t-form-item :label="k" :name="k">
+              <t-input v-model="drawer.dynamicForm[k]" :type="typeof drawer.dynamicForm[k]" placeholder="请输入仓库名称"
+                       :maxlength="64" with="120"></t-input>
+            </t-form-item>
+          </div>
+        </t-form>
+      </t-space>
     </t-drawer>
   </div>
 </template>
@@ -200,9 +192,9 @@ export default Vue.extend({
         {
           title: '名称',
           align: 'left',
-          width: 220,
+          width: 180,
           ellipsis: true,
-          colKey: 'discoveredLabels.job',
+          colKey: 'alertName',
           fixed: 'left',
         },
         {
@@ -210,38 +202,37 @@ export default Vue.extend({
           width: 120,
           ellipsis: true,
           fixed: 'left',
-          colKey: 'scrapePool',
+          colKey: 'exporterType',
         },
         {
           title: '状态',
-          colKey: 'health',
+          colKey: 'status',
           align: 'center',
           width: 60
         },
         {
-          title: '标签',
-          width: 200,
-          ellipsis: true,
-          colKey: 'labels',
-        },
-        {
-          title: '超时',
-          width: 60,
-          ellipsis: true,
+          title: '等级',
+          colKey: 'alertLevel',
           align: 'center',
-          colKey: "scrapeTimeout"
+          width: 60
         },
         {
-          title: '端点地址',
-          width: 200,
+          title: '分组',
+          width: 120,
           ellipsis: true,
-          colKey: "globalUrl"
+          colKey: 'groupName',
         },
         {
           title: '描述',
-          width: 180,
+          width: 150,
           ellipsis: true,
-          colKey: "lastError"
+          colKey: "description"
+        },
+        {
+          title: '触发时间',
+          width: 160,
+          ellipsis: true,
+          colKey: "createTime"
         },
         {
           align: 'left',
@@ -293,18 +284,18 @@ export default Vue.extend({
       // 当前数据
       formData: {
         id: 0,
-        url: "",
-        name: "",
-        icon: "",
-        type: "",
-        home: "",
+        alertName: "",
+        exporterType: "",
+        alertLevel: "",
+        groupName: "",
         description: "",
         createTime: "",
         updateTime: "",
         createBy: "",
+        createdByUserName: "",
         updateBy: "",
+        updateByUserName: "",
         status: "",
-        values: ""
       },
       typeList: []
     };
@@ -326,7 +317,7 @@ export default Vue.extend({
   created() {
     this.page();
   },
-  watch:{
+  watch: {
     "searchForm.job"(newVal, oldVal) {
       if (newVal != oldVal) {
         this.page()
@@ -360,7 +351,7 @@ export default Vue.extend({
     onChange(pageInfo) {
       console.log('Page Info: ', pageInfo);
     },
-    handleClickDetail(row:any) {
+    handleClickDetail(row: any) {
       this.formData = row;
       this.drawer.operation = 'detail';
       this.drawer.visible = true;
@@ -376,20 +367,19 @@ export default Vue.extend({
     },
     onConfirmDelete() {
       switch (this.confirm.operation) {
-          case 'delete':
-            // 请求删除
-            this.$request.delete("/prometheus/delete?id="+this.formData.id, {
-            }).then(res => {
-              if (res.data.code == 200) {
-                this.$message.success(res.data.msg);
-                this.confirm.visible = false;
-                this.page();
-              } else {
-                this.$message.error(res.data.msg);
-              }
-            }).catch(err => {
-            })
-            break;
+        case 'delete':
+          // 请求删除
+          this.$request.delete("/prometheus/delete?id=" + this.formData.id, {}).then(res => {
+            if (res.data.code == 200) {
+              this.$message.success(res.data.msg);
+              this.confirm.visible = false;
+              this.page();
+            } else {
+              this.$message.error(res.data.msg);
+            }
+          }).catch(err => {
+          })
+          break;
       }
     },
     onCancel() {
@@ -408,10 +398,10 @@ export default Vue.extend({
     },
     handleDrawerOk() {
       switch (this.drawer.operation) {
-          case 'add':
-            break;
-          case 'edit':
-            break;
+        case 'add':
+          break;
+        case 'edit':
+          break;
       }
     },
     // 取消抽屉
@@ -433,8 +423,8 @@ export default Vue.extend({
     page() {
       this.dataLoading = true;
       this.$request.get('/prometheus/alert/page', {
-          params: this.searchForm
-        }).then((res) => {
+        params: this.searchForm
+      }).then((res) => {
         if (res.data.code === 200) {
           this.data = res.data.rows;
           this.pagination.total = res.data.total;
