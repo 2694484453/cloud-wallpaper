588688c78113a9a6f05a456c5e5429bf59d43b92
gaopuguang <2694484453@qq.com>
2026-02-06 02:53:38 +0800

    调整

--- src/config/host.ts
+++ src/config/host.ts
@@ -1,57 +1,9 @@
 export default {
-  web_dev: {
-    // 开发环境接口请求
-    NAME: "web_dev",
-    API: 'http://localhost:9099',
-    PATH: "/api", //"/dev-api/",
-    // 开发环境 cdn 路径
-    CDN: '',
-    changeOrigin: true
-  },
-  web: {
-    // 生产环境接口请求
-    NAME: "web",
-    API: 'https://cloud-server.gpg123.vip',
-    PATH: "/api", //"/prod-api/",
-    // 开发环境 cdn 路径
-    CDN: '',
-    changeOrigin: true
-  },
-  test: {
-    NAME: "test",
-    // 测试环境接口地址
-    API: 'http://ecs.gpg123.vip:9099',
-    // 测试环境 cdn 路径
-    PATH: "",
-    CDN: '',
-    changeOrigin: true
-  },
   wallpaper: {
     NAME: "wallpaper",
-    API: 'https://cloud-server.gpg123.vip',
-    PATH: "/api",
-    CDN: '',
-    changeOrigin: true
-  },
-  wallpaper_dev: {
-    NAME: "wallpaper_dev",
     API: 'http://localhost:9099',
     PATH: "/api",
     CDN: '',
     changeOrigin: true
   },
-  tools_prod: {
-    NAME: "tools_prod",
-    API: 'https://cloud-server.gpg123.vip',
-    PATH: "/api",
-    CDN: '',
-    changeOrigin: true
-  },
-  tools_dev: {
-    NAME: "tools_dev",
-    API: 'http://localhost:9099',
-    PATH: "/api",
-    CDN: '',
-    changeOrigin: true
-  }
 };
