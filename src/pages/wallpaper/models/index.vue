861ab40ac28e015509574c3a0e767e11dd7a0254
gaopuguang <2694484453@qq.com>
2026-03-05 01:06:25 +0800

    调整图标

--- src/pages/wallpaper/models/index.vue
+++ src/pages/wallpaper/models/index.vue
@@ -1,91 +1,133 @@
 <template>
   <div>
-    <t-card class="list-card-container" :bordered="false">
-      <t-form
-        ref="form"
-        :data="formData"
-        :label-width="80"
-        colon
-        @reset="onReset"
-        @submit="page"
-        :style="{ marginBottom: '8px' }"
-      >
-      <t-row justify="space-between">
-        <div class="left-operation-container">
-<!--          <t-button @click="handleSetupContract"> 新建合同 </t-button>-->
-<!--          <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出合同 </t-button>-->
-<!--          <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>-->
-        </div>
-        <t-col :span="3">
-          <t-input v-model="searchForm.modelName" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
-            <template #suffix-icon>
-              <search-icon size="20px"/>
-            </template>
-          </t-input>
-        </t-col>
-        <t-col :span="2" class="operation-container">
-          <t-button theme="primary" type="submit" :style="{ marginLeft: '8px' }"> 查询</t-button>
-          <t-button type="reset" variant="base" theme="default"> 重置</t-button>
-        </t-col>
-      </t-row>
-      </t-form>
-      <div class="table-container">
-        <t-table
-          :columns="columns"
-          :data="data"
-          :rowKey="rowKey"
-          :verticalAlign="verticalAlign"
-          :hover="hover"
-          :pagination="pagination"
-          :selected-row-keys="selectedRowKeys"
-          :loading="dataLoading"
-          @page-change="rehandlePageChange"
-          @change="rehandleChange"
-          @select-change="rehandleSelectChange"
-          :headerAffixedTop="true"
-          :headerAffixProps="{ offsetTop: offsetTop, container: getContainer }"
+      <t-card class="list-card-container" :bordered="false">
+        <t-form
+          ref="form"
+          :data="formData"
+          :label-width="80"
+          colon
+          @reset="onReset"
+          @submit="page"
+          :style="{ marginBottom: '8px' }"
         >
-          <template #status="{ row }">
-            <t-tag v-if="row.status === CONTRACT_STATUS.FAIL" theme="danger" variant="light">审核失败</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.AUDIT_PENDING" theme="warning" variant="light">待审核</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.EXEC_PENDING" theme="warning" variant="light">待履行</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.EXECUTING" theme="success" variant="light">履行中</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.FINISH" theme="success" variant="light">已完成</t-tag>
+        <t-row justify="space-between">
+          <div class="left-operation-container">
+  <!--          <t-button @click="handleSetupContract"> 新建合同 </t-button>-->
+  <!--          <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出合同 </t-button>-->
+  <!--          <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>-->
+          </div>
+          <t-col :span="3">
+            <t-input v-model="searchForm.modelName" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
+              <template #suffix-icon>
+                <search-icon size="20px"/>
+              </template>
+            </t-input>
+          </t-col>
+          <t-col :span="2" class="operation-container">
+            <t-button theme="primary" type="submit" :style="{ marginLeft: '8px' }"> 查询</t-button>
+            <t-button type="reset" variant="base" theme="default"> 重置</t-button>
+          </t-col>
+        </t-row>
+        </t-form>
+        <div class="table-container">
+<!--          <t-table-->
+<!--            :columns="columns"-->
+<!--            :data="data"-->
+<!--            :rowKey="rowKey"-->
+<!--            :verticalAlign="verticalAlign"-->
+<!--            :hover="hover"-->
+<!--            :pagination="pagination"-->
+<!--            :selected-row-keys="selectedRowKeys"-->
+<!--            :loading="dataLoading"-->
+<!--            @page-change="rehandlePageChange"-->
+<!--            @change="rehandleChange"-->
+<!--            @select-change="rehandleSelectChange"-->
+<!--            :headerAffixedTop="true"-->
+<!--            :headerAffixProps="{ offsetTop: offsetTop, container: getContainer }"-->
+<!--          >-->
+<!--            <template #status="{ row }">-->
+<!--              <t-tag v-if="row.status === CONTRACT_STATUS.FAIL" theme="danger" variant="light">审核失败</t-tag>-->
+<!--              <t-tag v-if="row.status === CONTRACT_STATUS.AUDIT_PENDING" theme="warning" variant="light">待审核</t-tag>-->
+<!--              <t-tag v-if="row.status === CONTRACT_STATUS.EXEC_PENDING" theme="warning" variant="light">待履行</t-tag>-->
+<!--              <t-tag v-if="row.status === CONTRACT_STATUS.EXECUTING" theme="success" variant="light">履行中</t-tag>-->
+<!--              <t-tag v-if="row.status === CONTRACT_STATUS.FINISH" theme="success" variant="light">已完成</t-tag>-->
+<!--            </template>-->
+<!--            <template #url="{row}">-->
+<!--              <a :href="row.url" target="_blank">{{row.url}}</a>-->
+<!--            </template>-->
+<!--            <template #contractType="{ row }">-->
+<!--              <p v-if="row.contractType === CONTRACT_TYPES.MAIN">审核失败</p>-->
+<!--              <p v-if="row.contractType === CONTRACT_TYPES.SUB">待审核</p>-->
+<!--              <p v-if="row.contractType === CONTRACT_TYPES.SUPPLEMENT">待履行</p>-->
+<!--            </template>-->
+<!--            <template #paymentType="{ row }">-->
+<!--              <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.PAYMENT" class="payment-col">-->
+<!--                付款-->
+<!--                <trend class="dashboard-item-trend" type="up" />-->
+<!--              </p>-->
+<!--              <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.RECEIPT" class="payment-col">-->
+<!--                收款-->
+<!--                <trend class="dashboard-item-trend" type="down" />-->
+<!--              </p>-->
+<!--            </template>-->
+<!--            <template #op="slotProps">-->
+<!--              <a class="t-button-link" @click="handleClickDetail(slotProps.row)">详情</a>-->
+<!--              <a class="t-button-link" @click="handleClickDownload(slotProps.row)">加速下载</a>-->
+<!--  &lt;!&ndash;            <a class="t-button-link" @click="handleClickDelete(slotProps)">删除</a>&ndash;&gt;-->
+<!--            </template>-->
+<!--          </t-table>-->
+          <template >
+              <div class="list-card-items">
+                <t-row :gutter="[16, 16]">
+                  <t-col
+                    :lg="4"
+                    :xs="6"
+                    :xl="2"
+                    v-for="(item,index) in data"
+                    :key="index"
+                  >
+                    <t-card :bordered="true" :hover-shadow="true">
+                      <template #title>
+                        <span style="font-size: 12px">{{item.modelName+'-'+item.modelVersion}}</span>
+                      </template>
+                      <template #cover>
+                        <t-image :src="item.imageView+'?x-oss-process=image/resize,p_50'" />
+                      </template>
+                      <template #actions>
+                        <t-dropdown :options="options" :min-column-width="112" @click="clickHandler">
+                          <div class="tdesign-demo-dropdown-trigger">
+                            <t-button variant="text" shape="square">
+                              <more-icon />
+                            </t-button>
+                          </div>
+                        </t-dropdown>
+                      </template>
+                      <template #footer>
+                        <t-button theme="default">
+                          <heart-icon />readme
+                        </t-button>
+<!--                        <t-button theme="primary" variant="base">-->
+<!--                          <chat-icon />-->
+<!--                        </t-button>-->
+                        <t-button theme="primary">
+                          <share-icon />download
+                        </t-button>
+                      </template>
+                    </t-card>
+                  </t-col>
+                </t-row>
+              </div>
           </template>
-          <template #url="{row}">
-            <a :href="row.url" target="_blank">{{row.url}}</a>
-          </template>
-          <template #contractType="{ row }">
-            <p v-if="row.contractType === CONTRACT_TYPES.MAIN">审核失败</p>
-            <p v-if="row.contractType === CONTRACT_TYPES.SUB">待审核</p>
-            <p v-if="row.contractType === CONTRACT_TYPES.SUPPLEMENT">待履行</p>
-          </template>
-          <template #paymentType="{ row }">
-            <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.PAYMENT" class="payment-col">
-              付款
-              <trend class="dashboard-item-trend" type="up" />
-            </p>
-            <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.RECEIPT" class="payment-col">
-              收款
-              <trend class="dashboard-item-trend" type="down" />
-            </p>
-          </template>
-          <template #op="slotProps">
-            <a class="t-button-link" @click="handleClickDetail(slotProps.row)">详情</a>
-            <a class="t-button-link" @click="handleClickDownload(slotProps.row)">加速下载</a>
-<!--            <a class="t-button-link" @click="handleClickDelete(slotProps)">删除</a>-->
-          </template>
-        </t-table>
-      </div>
-    </t-card>
-    <t-dialog
-      header="确认删除当前所选合同？"
-      :body="confirmBody"
-      :visible.sync="confirmVisible"
-      @confirm="onConfirmDelete"
-      :onCancel="onCancel"
-    >
-    </t-dialog>
+        </div>
+      </t-card>
+      <t-dialog
+        header="确认删除当前所选合同？"
+        :body="confirmBody"
+        :visible.sync="confirmVisible"
+        @confirm="onConfirmDelete"
+        :onCancel="onCancel"
+      >
+      </t-dialog>
   </div>
 </template>
 <script lang="ts">
@@ -95,10 +137,12 @@ import Trend from '@/components/trend/index.vue';
 import { prefix } from '@/config/global';
 
 import { CONTRACT_STATUS, CONTRACT_STATUS_OPTIONS, CONTRACT_TYPES, CONTRACT_PAYMENT_TYPES } from '@/constants';
+import ProductCard from "@/components/product-card/index.vue";
 
 export default Vue.extend({
   name: 'ListBase',
   components: {
+    ProductCard,
     SearchIcon,
     Trend,
   },
